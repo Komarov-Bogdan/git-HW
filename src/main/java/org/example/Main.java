865341c52cb869e7dfcfdca88e6c9a2df1142b05
Jack Reacher <Komarov-Bogdan>
2024-04-01 16:46:47 +0300

    Java Automation Course: Homework#2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,4 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Automation Course: Homework#2");
+        System.out.println("Hello world!!!!");
+    }
+}
